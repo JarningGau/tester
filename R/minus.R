863005d1a0d338acb393da78bcfa35fe8a4f01bf
Jarning Gau <jarninggau@gmail.com>
2023-06-08 11:59:17 +0800

    feat: add minus function

---
+++ R/minus.R
@@ -0,0 +1,14 @@
+#' Returns the differ of two numbers
+#'
+#' @param a Number one.
+#' @param b Number two.
+#'
+#' @return Differ of two numbers
+#' @export
+#'
+#' @examples
+#' library(tester)
+#' minus(1, 2)
+minus <- function(a, b) {
+  a - b
+}
